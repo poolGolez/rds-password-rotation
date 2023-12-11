dfa89d84612bc4c56f9c9fa47f5a8f5674995bf8
Pool Golez <loop.edward@gmail.com>
2023-12-11 14:48:00 +0800

    Add SSM parameter for RDS

--- lib/rds-password-rotation-stack.ts
+++ lib/rds-password-rotation-stack.ts
@@ -1,16 +1,14 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 export class RdsPasswordRotationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    const queue = new sqs.Queue(this, 'RdsPasswordRotationQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const rdsPasswordParameter = new ssm.StringParameter(this, "RdsPassword", {
+      stringValue: "my-secret-password",
+      parameterName: "/rds-password-rotation-stack/dev/rds/password",
     });
   }
 }
