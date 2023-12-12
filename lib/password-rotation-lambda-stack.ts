4548c7af014d32ac021fde0f91245a75ede84a00
Pool Golez <loop.edward@gmail.com>
2023-12-12 17:43:01 +0800

    Rotate password schedule every 30 days

--- lib/password-rotation-lambda-stack.ts
+++ lib/password-rotation-lambda-stack.ts
@@ -1,11 +1,19 @@
 import { Construct } from "constructs";
 import * as cdk from "aws-cdk-lib";
 import * as nodeJs from "aws-cdk-lib/aws-lambda-nodejs";
+import * as events from "aws-cdk-lib/aws-events";
+import { Function } from "aws-cdk-lib/aws-lambda";
+import { LambdaFunction } from "aws-cdk-lib/aws-events-targets";
 
 export default class PasswordRotationLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
     const lambda = new nodeJs.NodejsFunction(this, "handler");
+
+    const schedule = new events.Rule(this, 'RotationSchedule', {
+      schedule: events.Schedule.rate(cdk.Duration.days(30))
+    })
+    schedule.addTarget(new LambdaFunction(lambda))
   }
 }
