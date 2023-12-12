aa1d555cc28d05f2e823ff64a356ee6c77d52aa2
Pool Golez <loop.edward@gmail.com>
2023-12-12 08:25:33 +0800

    Extract database construct for RDS

---
+++ lib/constructs/rds-construct.ts
@@ -0,0 +1,48 @@
+import {
+  InstanceType,
+  InstanceClass,
+  InstanceSize,
+  SubnetType,
+  IVpc,
+  SecurityGroup,
+  Peer,
+  Port,
+} from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
+import { SecretValue, RemovalPolicy } from "aws-cdk-lib";
+import {
+  Credentials,
+  DatabaseInstance,
+  DatabaseInstanceEngine,
+} from "aws-cdk-lib/aws-rds";
+
+export class PostgresRds extends Construct {
+  constructor(scope: Construct, id: string, props: PostgresRdsProps) {
+    super(scope, id);
+
+    const credentials = Credentials.fromPassword(
+      "root",
+      new SecretValue(props.password)
+    );
+
+    const dbAccessSecGroup = new SecurityGroup(this, "DbAccess", { vpc: props.vpc });
+    dbAccessSecGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(5432), "Allow Postgress access");
+    const securityGroups = [dbAccessSecGroup];
+
+    new DatabaseInstance(this, id, {
+      engine: DatabaseInstanceEngine.POSTGRES,
+      databaseName: "app",
+      securityGroups,
+      credentials,
+      removalPolicy: RemovalPolicy.DESTROY,
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.NANO),
+      vpc: props.vpc,
+      vpcSubnets: { subnetType: SubnetType.PUBLIC },
+    });
+  }
+}
+
+export interface PostgresRdsProps {
+  password: string;
+  vpc: IVpc;
+}
--- lib/rds-password-rotation-stack.ts
+++ lib/rds-password-rotation-stack.ts
@@ -1,35 +1,26 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ssm from "aws-cdk-lib/aws-ssm";
-import * as rds from "aws-cdk-lib/aws-rds";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { PostgresRds } from "./constructs/rds-construct";
 
 export class RdsPasswordRotationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const rdsPasswordParameter = new ssm.StringParameter(this, "RdsPassword", {
-      stringValue: "my-secret-password",
+      stringValue: "change-me-now",
       parameterName: "/rds-password-rotation-stack/dev/rds/password",
     });
 
     const vpc = ec2.Vpc.fromLookup(this, "DefaultVpc", {
       vpcId: "vpc-824693e4",
-      // isDefault: true,
     });
-    console.log({
-      publicSubnets: vpc.publicSubnets,
-      privateSubnets: vpc.privateSubnets
+
+    const rds = new PostgresRds(this, 'Database', {
+      password: rdsPasswordParameter.stringValue,
+      vpc: vpc
     })
 
-    new rds.DatabaseInstance(this, "PostgresInstance1", {
-      engine: rds.DatabaseInstanceEngine.POSTGRES,
-      // Generate the secret with admin username `postgres` and random password
-      credentials: rds.Credentials.fromGeneratedSecret("postgres"),
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
-      vpc,
-      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC }
-    });
   }
 }
