c0e1035318f1e13e8d80834f4916e611775f5248
Pool Golez <loop.edward@gmail.com>
2023-12-12 20:21:41 +0800

    Implement overwriting of password SSM parameter

--- lib/password-rotation-lambda-stack.handler.ts
+++ lib/password-rotation-lambda-stack.handler.ts
@@ -1,6 +1,25 @@
-import { Handler } from 'aws-lambda';
+import { Handler } from "aws-lambda";
+import { SSMClient, PutParameterCommand } from "@aws-sdk/client-ssm";
+
+const client = new SSMClient({ region: "ap-southeast-1" });
 
 export const handler: Handler = async (event, context) => {
-    console.log('EVENT: \n' + JSON.stringify(event, null, 2));
-    return context.logStreamName;
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+  const password = generatePassword();
+  console.log(`New password: ${password}`);
+
+  const command = new PutParameterCommand({
+    // TODO: Environment variable injected
+    Name: "/rds-password-rotation-stack/dev/rds/password",
+    Value: password,
+    Overwrite: true,
+  });
+  const response = await client.send(command);
+  console.log("Parameter Store Version:", response.Version)
+
+  return context.logStreamName;
 };
+
+function generatePassword(): string {
+  return new Date().getTime().toString();
+}
